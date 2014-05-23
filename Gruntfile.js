12fa2fe4e152e7da7389bd1e3f2e0d48a5601704
Philippe Vachon-Rivard <Philippe.Vachon-Rivard@iprospect.com>
2014-05-23 17:02:09 -0400

    Added support for copying and newer

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,173 +1,157 @@
 module.exports = function(grunt) {
 
+  var Toolbox = {};
 
-	console.log("");
-	console.log("========================================");
-	console.log("================ TOOLBOX ===============");
-	console.log("========================================");
-	console.log("");
+  Toolbox.config = {
+    debug:      grunt.option("debug")   || null,
+    src:        grunt.option("src")     || "src/",
+    dest:       grunt.option("dest")    || "dist/",
+    backup:     grunt.option("backup")  || "backup/",
+    temp:       grunt.option("temp")    || "temp/",
+    imgSrc:     grunt.option("imgSrc")  || "src/img/",
+    imgDest:    grunt.option("imgDest") || "dist/img/",
+    imgBackup:  grunt.option("imgBackup") || "backup/img/",
+    jsBackup:   grunt.option("jsBackup") || "backup/js/",
+    jsSrc:      grunt.option("jsSrc")   || "src/js/",
+    jsDest:     grunt.option("jsDest")  || "dist/js/",
+  };
 
-	var sourceFile = grunt.option('sourceFile') || '';
-	var sourceFolderPath = sourceFile;
-	sourceFolderPath = sourceFolderPath.substr(0, sourceFolderPath.lastIndexOf("/"));
-	sourceFolderPath =  sourceFolderPath;
-	var sourceFileName =  sourceFile.split('/').pop();
-	var destinationFile = grunt.option('destinationFile') || '';
+  Toolbox.jsSrcPath   = function(){return Toolbox.config.jsSrc};
+  Toolbox.jsDestPath  = function(){return Toolbox.config.jsDest};
+  Toolbox.imgSrcPath  = function(){return Toolbox.config.imgSrc};
+  Toolbox.imgDestPath = function(){return Toolbox.config.imgDest};
+  Toolbox.imgBckPath  = function(){return Toolbox.config.imgBackup};
+  Toolbox.BckPath     = function(){return Toolbox.config.backup};
 
+  if(Toolbox.config.debug){
+    console.log("Src : " + Toolbox.config.src);
+    console.log("Dest : " + Toolbox.config.dest);
+    console.log("imgSrc : " + Toolbox.config.imgSrc);
+    console.log("imgDest : " + Toolbox.config.imgDest);
+    console.log("jsSrc : " + Toolbox.config.jsSrc);
+    console.log("jsDest : " + Toolbox.config.jsDest);
+  }
 
-	
-	var quality = grunt.option('quality') || '8';
-	var extensionJPG = grunt.option('extensionJPG') || '.jpg';
-	var extensionPNG = grunt.option('extensionPNG') || '.png';
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    imagemin: {
+      dynamic: {
+        options: {
+          optimizationLevel: 7,
+          progressive: true,
+          interlaced: true
+        },
+        files: [{
+          expand: true,   
+          cwd: Toolbox.imgSrcPath(),
+          src: ['**/*.{png,jpg,gif,jpeg}'],      
+          dest: Toolbox.imgDestPath(),  
+        }]
+      }
+    }, 
+    copy: {
+      images: {
+        files: [
+          {
+            expand: true, 
+            cwd: Toolbox.imgSrcPath(),
+            src: ["**"], 
+            dest: Toolbox.imgDestPath()
+          }
+        ]
+      },
+      backupimages: {
+        files: [
+          {
+            expand: true,
+            cwd: Toolbox.imgSrcPath(),
+            src: ["**"],
+            dest: Toolbox.imgBckPath()
+          }
+        ]
+      }
+    },
+    clean: {
+      images: [Toolbox.imgDestPath()],
+      js: [Toolbox.jsDestPath()],
+      imgBackup: [Toolbox.imgBckPath()],
+      backup: [Toolbox.BckPath()],
+    },
+    concat: {
+        options: {
+          separator: "\n", //add a new line after each file
+          banner: "", //added before everything
+          footer: "" //added after everything
+        },
+        dist: {
+          // the files to concatenate
+          src: [
+            //include libs
+            Toolbox.jsSrcPath() + 'jquery.colorbox-min.js',
+            Toolbox.jsSrcPath() + 'jquery.cycle.js',
+            ],
+          // the location of the resulting JS file
+          dest: Toolbox.jsDestPath() + 'scripts.js'
+        }
+      },
+      uglify: {
+        build: {
+          files: [{
+            expand: true,
+            cwd: Toolbox.jsDestPath(),
+            src: ['scripts.js'],
+            dest: Toolbox.jsDestPath(),
+            ext: '.js'
+          }]
+        }
+      },
+      watch: {
+        scripts: {
+          files: [Toolbox.jsSrcPath() + '*.js'],
+          tasks: ['concat:dist', 'uglify'],
+          options: {
+            interrupt: true
+          }
+        }, 
+        newimages: {
+          files: ["**/*.{png,jpg,jpeg,gif}"],
+          tasks: ['compress-new-img'],
+          options: {
+            cwd: Toolbox.imgSrcPath()
+          }
+        }
+      }
+  });
 
-	var detectedFileType = ".jpg"
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-newer');
 
-	var destinationFolder = grunt.option('destinationFolder') || '';
-
-	if(sourceFile.indexOf(".jpg") > 0){
-		detectedFileType= "jpg";
-	}else if(sourceFile.indexOf(".png") > 0){
-		detectedFileType= "png";
-	}else{
-		detectedFileType = "";
-	}
-
-	if(detectedFileType == "jpg"){
-		console.log("= JPG Compression Command Detected");
-		console.log("--sourceFile : " + sourceFile);
-
-
-	destinationFile = destinationFile.substr(0, destinationFile.lastIndexOf("/"));
-
-
-
-		console.log("--destinationFile : " + destinationFile);
-
-		console.log("--quality : " + quality);
-
-		console.log("--extensionJPG : " + extensionJPG);
-		console.log("--extensionPNG : " + extensionPNG);
-		console.log("");
-
-		grunt.initConfig({
-			pkg: grunt.file.readJSON('package.json'),
-			imagemin: {
-				jpg: {
-					options: {
-						progressive: true,
-						optimizationLevel: quality
-					},
-					files: [
-					{
-						expand: true,
-						cwd: sourceFolderPath,
-						src: sourceFileName,
-						dest: destinationFile
-					}
-					]
-				}
-			}
-		});
-	}else if(detectedFileType == "png"){
-		console.log("= PNG Compression Command Detected");
-		console.log("--sourceFile : " + sourceFile);
-
-		destinationFile = destinationFile.substr(0, destinationFile.lastIndexOf("/"));
-		console.log("--destinationFile : " + destinationFile);
-
-		console.log("--quality : " + quality);
-
-		console.log("--extensionJPG : " + extensionJPG);
-		console.log("--extensionPNG : " + extensionPNG);
-		console.log("");
-
-
-		grunt.initConfig({
-			pkg: grunt.file.readJSON('package.json'),
-			imagemin: {
-				png: {
-					options: {
-						optimizationLevel: quality
-					},
-					files: [
-					{
-						expand: true,
-						cwd: sourceFolderPath,
-						src: sourceFileName,
-						dest: destinationFile
-					}
-					]
-				}
-			}
-		});
-	}else{
-		console.log("= Javascript Compression Command Detected");
-		
-
-		sourceFolderPath = grunt.option('sourceFolderPath') || 'working/js/';
-		destinationFolder = grunt.option('destinationFolder') || 'working/js_concat/';
-
-		console.log("--sourceFolderPath : " + sourceFolderPath);
-		console.log("--destinationFolder : " + destinationFolder);
-
-		grunt.initConfig({
-			pkg: grunt.file.readJSON('package.json'),
-			concat: {
-				options: {
-				separator: "\n", //add a new line after each file
-				banner: "", //added before everything
-				footer: "" //added after everything
-			},
-			dist: {
-				// the files to concatenate
-				src: [
-					//include libs
-					sourceFolderPath + 'jquery.colorbox-min.js',
-					sourceFolderPath + 'jquery.cycle.js',
-					],
-				// the location of the resulting JS file
-				dest: destinationFolder + 'scripts.js'
-			}
-		},
-		uglify  : {
-			build: {
-				files: [
-				{
-					expand: true,
-					cwd: destinationFolder,
-					src: ['scripts.js'],
-					dest: destinationFolder,
-					ext: '.js'
-				}
-				]
-			}
-		},
-		watch: {
-			scripts: {
-				files: [sourceFolderPath + '*.js'],
-				tasks: ['concat:dist', 'uglify'],
-				options: {
-					interrupt: true
-				}
-			}
-		}
-	});	
-	}
-	
-	grunt.loadNpmTasks('grunt-contrib-imagemin');
-	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-
-	//register the task
-	grunt.registerTask('compress-images', ['imagemin']);
-	grunt.registerTask('compress-javascript', ['concat:dist']);
-	grunt.registerTask('compress-javascript', ['uglify:build']);
-
-
+  // Default task(s).
+  grunt.registerTask('default', ['compress-img', 'compress-js']);
+  grunt.registerTask('compress-and-watch-new-img', ['compress-img', 'watch:newimages']);
+  grunt.registerTask('compress-new-img', ['newer:copy:backupimages', 'newer:imagemin:dynamic']);
+  if(Toolbox.imgSrcPath() === Toolbox.imgDestPath()){
+    grunt.registerTask('compress-img', [
+      'clean:imgBackup',
+      'copy:backupimages',
+      'imagemin:dynamic'
+    ]);
+  } else {
+    grunt.registerTask('compress-img', [
+      'clean:images', 
+      'clean:imgBackup', 
+      'copy:backupimages',
+      'copy:images', 
+      'imagemin:dynamic'
+    ]);
+  }
+  grunt.registerTask('compress-js', ['clean:js','concat:dist', 'uglify:build']);
+  grunt.registerTask('cleanall', ['clean:images', 'clean:backup', 'clean:js']);
 
 };
-
-
-
-
